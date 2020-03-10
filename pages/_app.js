26fe1283ac51d80f42464f6f8a16ff04cf95f71b
Developer <Dev@jacobs-macbook-pro.home>
2020-03-10 20:03:44 +0100

    shadow on filter menu

--- pages/_app.js
+++ pages/_app.js
@@ -175,6 +175,7 @@ class ActivityApp extends App {
           .react-select__menu {
             margin-top: 0!important;
             border-radius: 0!important;
+            box-shadow: 0px 6px 6px -3px rgba(0,0,0,0.2), 0px 10px 14px 1px rgba(0,0,0,0.14), 0px 4px 18px 3px rgba(0,0,0,0.12);
           }
 
           .react-select__menu-list {

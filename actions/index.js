625139a510ed14e27c4030f02e895251a3237bc3
Developer <Dev@jacobs-macbook-pro.home>
2020-03-10 20:07:28 +0100

    add next.config.js

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = process.env.BASE_URL;
+// const BASE_URL = 'http://localhost:3000'
 // const BASE_URL = 'https://doozone.now.sh'
 const ACTIVITY_DATA = []
 const CATEGORY_DATA = [

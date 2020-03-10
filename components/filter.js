30a82676cde68ad9fdf3c3af4ac2063bbe2ded9b
Developer <Dev@jacobs-macbook-pro.home>
2020-03-10 19:51:48 +0100

    remove border from filter dropdown

--- components/filter.js
+++ components/filter.js
@@ -1,18 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Select from 'react-select';
 import { CATEGORY_LIST } from '../actions';
 
 const Filter = (props) => {
-
-  const [category, setCategory] = useState('')
-  const [hideLabel, setLabel] = useState(false)
-  // const classes = useStyles();
-
-  const handleChange = event => {
-    props.changeCategory(event.target.value)
-    setCategory(event.target.value)
-    setLabel(true)
-  };
 
   const onChange = value => {
     console.log(value)
@@ -20,30 +10,15 @@ const Filter = (props) => {
   };
 
   return (
-    <>
-      <Select 
-        className="react-select-container"
-        maxMenuHeight={550}
-        classNamePrefix="react-select"
-        onChange={onChange}
-        options={CATEGORY_LIST}
-        placeholder="Discover amazing experiences"
-        isSearchable={false}
-      />
-
-      <style jsx>{`
-        .react-select-container {
-          display: none!important;
-        }
-
-        .react-select__control {
-          width: 350px!important;
-        }
-      `}</style>
-
-
-
-    </>
+    <Select 
+      className="react-select-container"
+      maxMenuHeight={550}
+      classNamePrefix="react-select"
+      onChange={onChange}
+      options={CATEGORY_LIST}
+      placeholder="Discover amazing experiences"
+      isSearchable={false}
+    />
   );
 }
 

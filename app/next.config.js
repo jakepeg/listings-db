9af579f1bbfcd8be66a9702a19f1d6b1d5d08ec8
Oleh Botsiun <olegbotsiun@gmail.com>
2020-03-16 18:08:26 +0200

    feature/server: add readme

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const prod = process.env.NODE_ENV === 'production'
+require("dotenv").config();
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   env: {
-    BASE_URL: prod ? 'https://doozone.now.sh' : 'http://localhost:3000'
-  },
-}
+    BASE_URL: prod ? "https://doozone.now.sh" : "http://localhost:3001"
+  }
+};

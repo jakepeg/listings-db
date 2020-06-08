2550c2b5b5fa3c9790e5d2ac6067db10b3e0d078
Developer <Dev@jacobs-macbook-pro.home>
2020-06-08 15:21:59 +0200

    connect mongo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const config = require('./db_config');
+
+const mongoose = require('mongoose');
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err) => {
+  if (err) { console.error(err); }
+  else {
+    console.log('Connected to DB!');
+  }
+})
 
 const server = express();
 const port = 3001;
